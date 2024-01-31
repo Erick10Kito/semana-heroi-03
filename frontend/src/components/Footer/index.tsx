293e47e6de47b267194c7c02c2875ad6ee17daf5
Erick de Bem Campos <erickcamposbem69@gmail.com>
2024-01-31 07:59:21 -0300

    current dynamic date added in footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,13 +2,16 @@ import { Camera, Computer, Mic, Phone } from "@/Icons";
 import Container from "../Container";
 
 export default function Footer() {
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, '0') + ':'
+    const minutes = date.getMinutes.toString().padStart(2, '0')
 
     return(
         <div className="fixed bottom-0 bg-black py-6 w-full">
             <Container>
                 <div className="grid grid-cols-3">
                         <div className="flex items-center">
-                            <p className="text-xl">10:34</p>
+                            <p className="text-xl">{hours}{minutes}</p>
                         </div>
                         <div className="flex space-x-6 justify-center">
                             <Mic className="w-16 h-10 text-white p-2 bg-gray-950 rounded-md "/>
